0f22443920e1b6b46c0676573ea7fd10aaebee55
AceChen <1263464197@qq.com>
2017-07-23 23:48:23 +0800

    scroll 组件 + loading 组件

---
+++ vue-music/src/base/loading/loading.vue
@@ -0,0 +1,37 @@
+<!--
+  * author: AceChen
+  * 2017-7-23
+  * readme: loading 加载组件
+-->
+<template>
+	<div ref="loading">
+        <img width="24" height="24" src="./loading.gif"/>
+        <p class="desc">{{title}}</p>
+    </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+
+export default {
+	props: {
+		title: {
+            type: String,
+            default: '正在载入...'
+        }
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .loading{
+        width: 100%;
+        text-align: center;
+    }
+    .desc{
+      line-height: 20px;
+      font-size: 12px;
+      color: #ccc;
+    }
+</style>
---
+++ vue-music/src/base/scroll.vue
@@ -0,0 +1,72 @@
+<!--
+  * author: AceChen
+  * 2017-7-23
+  * readme: scroll 滚动组件
+-->
+<template>
+	<div ref="wrapper">
+        <!--当做一个插槽使用-->
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+
+export default {
+	props: {
+		probeType: {
+            type: Number,
+            default: 1
+        },
+        click: {
+            type: Boolean,
+            default: true
+        },
+        data: {
+            type: Array,
+            default: null
+        }
+	},
+
+	mounted() {
+		setTimeout(() => {
+            this._initScroll()
+        }, 20)
+	},
+
+	methods: {
+		_initScroll() {
+            if(!this.$refs.wrapper) {
+                return
+            }
+            this.scroll = new BScroll(this.$refs.wrapper, {
+                probeType: this.probeType,
+                click: this.click
+            })
+        },
+        enable() {
+            this.scroll && this.scroll.enable()
+        },
+        disable() {
+            this.scroll && this.scroll.disable()
+        },
+        refresh() {
+            this.scroll && this.scroll.refresh()
+        },
+	},
+
+    watch: {
+        data() {
+            setTimeout(() => {
+                this.refresh()
+            }, 20)
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
